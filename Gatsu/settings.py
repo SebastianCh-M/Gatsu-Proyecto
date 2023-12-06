01535b85a74368fefa4f524bbfaae38c49df87b9
SebastianCh-M <80702362+SebastianCh-M@users.noreply.github.com>
2023-12-05 22:51:38 -0300

    Archivos modificados

--- Gatsu/settings.py
+++ Gatsu/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['51.20.78.108', '0.0.0.0']
+ALLOWED_HOSTS = ['51.20.78.108', '0.0.0.0','127.0.0.1', 'localhost']
 
 
 # Application definition
